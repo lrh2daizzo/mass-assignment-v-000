08bd4aecc4732eb70b7072af1c87a21b55347655
Luis Herrera <herrera.lrh@gmail.com>
2020-10-06 00:33:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,6 @@ class Person
     :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    binding.pry
     attributes.each { |key, value| self.send("#{key}=", value)}
   end
 
